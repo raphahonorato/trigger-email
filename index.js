44d9b79ab945596e0fa61980c5c4e3e3128d2557
raphahonorato <raphaahonorato!gmail.com>
2022-12-23 14:02:46 -0300

    ajuste .env

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const nodemailer = require('nodemailer')
 const app = express()
-const port = 3000
+const port = PROCESS.ENV.PORT
 
 //credenciais do email de disparo
 const user = "master@cuidarecife.com.br"
