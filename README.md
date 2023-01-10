99377d11585ea730758e7a2df7e4a2ae729aa515
raphahonorato <raphaahonorato@gmail.com>
2023-01-10 08:18:07 -0300

    refatorando algumas coisas

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-## Disparador de email funcionante operante brabamente
+## Disparador de email
+
+Para instalar as depencias
+
+npm install
+
+
+Para executar o script
+
+npm start
+
+Para disparar o email acesse
+http://localhost:PORT
+
+DEFAULT=3000
+
+*editável no arquivo .env
+
+--------------
+
+Como disparar o email
+
+acesse a rota /send
+
+assim que é acessada o email é disparado automaticamente
--- index.js
+++ index.js
@@ -4,25 +4,27 @@ const app = express()
 const port = process.env.PORT
 
 //credenciais do email de disparo
-const user = "master@cuidarecife.com.br"
-const pass = "_f19|{6+hx62"
+const user = "EMAIL DE DISPARO"
+const pass = "SENHA"
 
 //página web onde está rodando a aplicação
 app.get('/', (req, res) => res.send('Hello, world'));
+
+//assim que acessada o email é disparado
 app.get('/send', (req, res) => {
 
     //credenciais SMTP da hospedagem 
     const transporter = nodemailer.createTransport({
-        host: "mail.cuidarecife.com.br",
-        port: 465,
+        host: "<DOMINIO HOST>",
+        port: <PORTA/>,
         auth: { user, pass }
     })
 
     //corpo do email que será enviado
     transporter.sendMail({
         from: user,                                   //quem enviou o email
-        to: "raphaahonorato@gmail.com",               //onde chegará o formulário preenchido do usuário
-        replyTo: "raphaelhonorato@legionschool.io",   //botão reply responde pra esse email
+        to: "<REMETENTE>",               //onde chegará o formulário preenchido do usuário
+        replyTo: "<REPLY TO>",   //botão reply responde pra esse email
         subject: "Teste de envio!!!",                 //assunto do email
         text: "Teste de conteúdo do email..",         //conteúdo do email --PODE SER INSERIDO HTML
     }).then(info => {
